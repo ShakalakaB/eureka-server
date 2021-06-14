e3c2fae41bd92eec650a5a5e02b0a65dcd91b2de
ShakalakaB <yunjie989@gmail.com>
2021-06-14 22:29:45 +0800

    Configure enreka server

---
+++ src/main/java/aldora/eureka/eurekaserver/config/EurekaConfig.java
@@ -0,0 +1,9 @@
+package aldora.eureka.eurekaserver.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaConfig {
+}
